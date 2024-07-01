84c212fe56a481d3cd4cc20885d2d1a2c71178eb
Walpurgisnatch <Kawemirs@gmail.com>
2024-07-01 21:06:10 +0700

    leds

--- sequencer.ino
+++ sequencer.ino
@@ -1,13 +1,16 @@
-#define STEPS_COUNT 2
+#define STEPS_COUNT 5
 
-#define CV_OUT 9
+// pins
+#define CV_OUT 3
 #define GATE_OUT 10
-#define TEMPO_BUTTON 4
+#define TEMPO_BUTTON 2
 #define TEMPO_KNOB 0
+#define LIGHT_STARTS 4
 
 unsigned long timer;
 unsigned long current_step_time;
 
+// vars
 int tempo = 750;
 int current_bpm = 80;
 
@@ -16,7 +19,6 @@ int to_bpm(int ms) {
 } 
 
 void setup() {
-  Serial.begin(9600);
   pinMode(TEMPO_BUTTON, INPUT);
   digitalWrite(TEMPO_BUTTON, HIGH);
 }
@@ -26,6 +28,14 @@ void loop() {
   byte read_tempo = 0;
   for (int i = 0; i < STEPS_COUNT; i++) {
     current_step_time = timer;
+
+    if (i == 0) {
+      digitalWrite(STEPS_COUNT + LIGHT_STARTS - 1, LOW);
+    } else {
+      digitalWrite(i + LIGHT_STARTS - 1, LOW); 
+    }
+    
+    digitalWrite(i + LIGHT_STARTS, HIGH);
     
     for(;;) {
       timer = millis();
@@ -34,16 +44,12 @@ void loop() {
       
       read_tempo = digitalRead(TEMPO_BUTTON);
       if (read_tempo == LOW) {
-        tempo = analogRead(TEMPO_KNOB) * 4;
+        tempo = analogRead(TEMPO_KNOB) * 2;
         current_bpm = to_bpm(tempo);
       } else {
         val = analogRead(i);
         analogWrite(CV_OUT, val/4);
       }
     }
-    Serial.print("tempo: ");
-    Serial.println(tempo);
-    Serial.print("value: ");
-    Serial.println(val);
   }
 }

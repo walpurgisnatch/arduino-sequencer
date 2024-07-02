e0b7993e86db52f1efe1722614813cf642199b1c
Walpurgisnatch <Kawemirs@gmail.com>
2024-07-02 10:16:38 +0700

    second tab

--- sequencer.ino
+++ sequencer.ino
@@ -1,4 +1,5 @@
 #define STEPS_COUNT 5
+#define TABS 2
 
 // pins
 #define CV_OUT 3
@@ -6,6 +7,9 @@
 #define TEMPO_BUTTON 2
 #define TEMPO_KNOB 0
 #define LIGHT_STARTS 4
+#define TAB_2 13
+#define TAB_2_LED 11
+#define SAVE_TAB 12
 
 unsigned long timer;
 unsigned long current_step_time;
@@ -14,42 +18,109 @@ unsigned long current_step_time;
 int tempo = 750;
 int current_bpm = 80;
 
+int steps[TABS][STEPS_COUNT];
+int current_tab = 0;
+
 int to_bpm(int ms) {
   return 60000 / ms;
-} 
+}
+
+void print_tabs() {
+  for (int tab = 0; tab < TABS; tab++) {
+    Serial.print("tab: ");
+    Serial.println(tab);
+    for (int i = 0; i < STEPS_COUNT; i++) {
+      Serial.print(steps[tab][i]);
+      Serial.print(",");
+    }
+    Serial.println();
+  }
+}
 
 void setup() {
+  Serial.begin(9600);
+  
   pinMode(TEMPO_BUTTON, INPUT);
   digitalWrite(TEMPO_BUTTON, HIGH);
+
+  pinMode(TAB_2, INPUT);
+
+  pinMode(SAVE_TAB, INPUT);
+  digitalWrite(SAVE_TAB, HIGH);
+
+  read_tab_cv(0);
+  read_tab_cv(1);
+}
+
+void step_led(int i) {
+  if (i == 0) {
+    digitalWrite(STEPS_COUNT + LIGHT_STARTS - 1, LOW);
+  } else {
+    digitalWrite(i + LIGHT_STARTS - 1, LOW); 
+  }
+  digitalWrite(i + LIGHT_STARTS, HIGH);
+}
+
+void read_tab_cv(int tab) {
+  for (int i = 0; i < STEPS_COUNT; i++) {
+    steps[tab][i] = analogRead(i);
+  }
+  Serial.println("tab saved");
 }
 
 void loop() {
+  int last = 0;
   int val = 0;
-  byte read_tempo = 0;
-  for (int i = 0; i < STEPS_COUNT; i++) {
-    current_step_time = timer;
+  for (int tab = 0; tab < TABS; tab++) {
+    Serial.print("tab: ");
+    Serial.println(tab);
+    for (int i = 0; i < STEPS_COUNT; i++) {
+      current_step_time = timer;
+      step_led(i);
 
-    if (i == 0) {
-      digitalWrite(STEPS_COUNT + LIGHT_STARTS - 1, LOW);
-    } else {
-      digitalWrite(i + LIGHT_STARTS - 1, LOW); 
-    }
-    
-    digitalWrite(i + LIGHT_STARTS, HIGH);
-    
-    for(;;) {
-      timer = millis();
-      if (timer - current_step_time >= tempo)
-        break;
-      
-      read_tempo = digitalRead(TEMPO_BUTTON);
-      if (read_tempo == LOW) {
-        tempo = analogRead(TEMPO_KNOB) * 2;
-        current_bpm = to_bpm(tempo);
+      if (digitalRead(TAB_2) == HIGH) {
+        current_tab = 1;
+        digitalWrite(TAB_2_LED, HIGH);
       } else {
-        val = analogRead(i);
-        analogWrite(CV_OUT, val/4);
+        current_tab = 0;
+        digitalWrite(TAB_2_LED, LOW);
       }
+
+      if (digitalRead(SAVE_TAB) == LOW)
+        read_tab_cv(current_tab);
+    
+      for(;;) {
+        timer = millis();
+        if (timer - current_step_time >= tempo)
+          break;
+
+        // read tempo input
+        if (digitalRead(TEMPO_BUTTON) == LOW) {
+          if (i == TEMPO_KNOB) {
+            analogWrite(CV_OUT, steps[tab][i]/4);
+          }
+          tempo = analogRead(TEMPO_KNOB) * 2;
+          current_bpm = to_bpm(tempo);
+        }
+
+        // cv out
+        if (tab == current_tab) {
+          val = analogRead(i);
+          if (val != last) {
+            last = val;
+            analogWrite(CV_OUT, val/4);
+          }
+        } else {
+          val = steps[tab][i];
+          if (val != last) {
+            last = val;
+            analogWrite(CV_OUT, val/4);
+          }
+        }
+      }
+      
+      Serial.print("cv: ");
+      Serial.println(val);
     }
   }
 }
